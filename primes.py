f2ebad4211f69c0095cba4dfe5ed2fa99e924f41
Annie Spahn <soccercat11@hotmail.com>
2016-06-02 17:21:26 -0600

    done

--- primes.py
+++ primes.py
@@ -1,8 +1,10 @@
-num = int(raw__input("Please enter a number: "))
-for i in range(2, num):
-      if num % i == 0:
-         print(str(num) + " is not a prime number")
-         break
-      else:
-         print(str(num) + " is a prime number")
-         break  
+import sys
+
+for p in range(int(sys.argv[1]), int(sys.argv[2])):
+     for i in range(2, p):
+            if p %  i == 0:
+               print(str(p) + " is not a prime number")
+               break
+            else:
+               print(str(p) + " is a prime number")
+               break  
