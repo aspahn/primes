1884581ce01858ec65462fc7b56806f34f77b9e0
Annie Spahn <soccercat11@hotmail.com>
2016-06-02 15:29:37 -0600

    first version

--- primes.py
+++ primes.py
@@ -1,2 +1,8 @@
-
-print "I have a lot of work to do"
+num = int(raw__input("Please enter a number: "))
+for i in range(2, num):
+      if num % i == 0:
+         print(str(num) + " is not a prime number")
+         break
+      else:
+         print(str(num) + " is a prime number")
+         break  
